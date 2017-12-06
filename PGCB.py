bf07cd5461f301635154b1c6214d9187fa880898
ali_cramstack <mazg1493@gmail.com>
2017-12-06 19:18:42 +0600

    some progress

--- PGCB.py
+++ PGCB.py
@@ -2,7 +2,7 @@ import os
 import pandas
 
 
-class PgcbFiles:
+class PgcbFiles(object):
     
     def __init__(self, actual_filename='actual.csv', probable_filename='probable.csv',
                  input_file_dir='/PGCBFiles/', output_file_dir='/PGCBOut/'):
@@ -12,29 +12,35 @@ class PgcbFiles:
         self.probable_file = self.output_path + probable_filename
 
     def list_of_input_files(self):
-        exists, files_list = self.input_directory_empty()
-        if exists:
+        empty, files_list = self.input_directory_empty()
+        if empty:
             raise Exception("Input directory is empty!")
         return files_list
 
     def list_of_processed_files(self):
-        exists, files_list = self.output_directory_empty()
-        if exists:
+        empty, files_list = self.output_directory_empty()
+        if empty:
             raise Exception("Input directory is empty!")
         return files_list
 
     def input_directory_empty(self):
         files_list = [file_item for file_item in os.listdir(self.input_path) if not file_item.startswith('.')]
         if not files_list:
-            return True, files_list
-        return False, []
+            return True, []
+        return False, files_list
 
     def output_directory_empty(self):
         files_list = [file_item for file_item in os.listdir(self.output_path) if not file_item.startswith('.')]
         if not files_list:
-            return True, files_list
-        return False, []
+            return True, []
+        return False, files_list
 
     @staticmethod
     def file_exists(path='', filename=''):
         return os.path.isfile(os.path.join(path, filename))
+
+
+class PandasProcess(object):
+
+    def __init__(self, filename, sheet_name):
+        pass
