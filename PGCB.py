64bd3c9a5d6f364449ac6d2f90df77c9be93cc50
--replace <mazg1493@gmail.com>
2017-12-06 23:54:43 +0600

    Some minor changes

--- PGCB.py
+++ PGCB.py
@@ -1,39 +1,45 @@
 import os
 import pandas
 
+from exception import EmptyDirectoryException
+
 
 class PgcbFiles(object):
     
-    def __init__(self, actual_filename='actual.csv', probable_filename='probable.csv',
-                 input_file_dir='/PGCBFiles/', output_file_dir='/PGCBOut/'):
+    def __init__(self, input_file_dir='/PGCBFiles/', output_file_dir='/PGCBOut/'):
         self.input_path = os.getcwd() + input_file_dir
         self.output_path = os.getcwd() + output_file_dir
-        self.actual_file = self.output_path + actual_filename
-        self.probable_file = self.output_path + probable_filename
+
+    def get_input_filepath(self):
+        return self.input_path
+
+    def get_output_filepath(self):
+        return self.output_path
 
     def list_of_input_files(self):
-        empty, files_list = self.input_directory_empty()
-        if empty:
-            raise Exception("Input directory is empty!")
+        files_list = self.directory_empty(self.input_path)
+        try:
+            if not files_list:
+                raise EmptyDirectoryException("Directory is empty!")
+        except EmptyDirectoryException as e:
+            print(e)
         return files_list
 
-    def list_of_processed_files(self):
-        empty, files_list = self.output_directory_empty()
-        if empty:
-            raise Exception("Input directory is empty!")
+    def list_of_output_files(self):
+        files_list = self.directory_empty(self.output_path)
+        try:
+            if not files_list:
+                raise EmptyDirectoryException("Directory is empty!")
+        except EmptyDirectoryException as e:
+            print(e)
         return files_list
 
-    def input_directory_empty(self):
-        files_list = [file_item for file_item in os.listdir(self.input_path) if not file_item.startswith('.')]
-        if not files_list:
-            return True, []
-        return False, files_list
-
-    def output_directory_empty(self):
-        files_list = [file_item for file_item in os.listdir(self.output_path) if not file_item.startswith('.')]
-        if not files_list:
-            return True, []
-        return False, files_list
+    @staticmethod
+    def directory_empty(filepath):
+        try:
+            return [file_item for file_item in os.listdir(filepath) if not file_item.startswith('.')]
+        except OSError as e:
+            print(e)
 
     @staticmethod
     def file_exists(path='', filename=''):
